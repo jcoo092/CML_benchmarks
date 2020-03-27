79f418f694bff2191606421703ff98cea1a80a61
James Cooper <30380166+jcoo092@users.noreply.github.com>
2020-03-27 17:31:51 +1300

    Added use of unsafe ops
    Again, can't tell if it made any difference to the speed...

--- Racket/montecarlopi.rkt
+++ Racket/montecarlopi.rkt
@@ -2,15 +2,16 @@
 
 (provide main)
 (require racket/flonum)
+(require racket/unsafe/ops)
 
 (define (montecarlopi randomiser iterations return-chan)
   (define (helper accumulator iteration)
     (match iteration
       [0 accumulator]
-      [iter (let ([x (random randomiser)] [y (random randomiser)] [next-iter (- iter 1)])
-              (let ([in-circle (fl+ (fl* x x) (fl* y y))])
-                (if (fl< in-circle 1.0)
-                    (helper (+ accumulator 1) next-iter)
+      [iter (let ([x (random randomiser)] [y (random randomiser)] [next-iter (unsafe-fx- iter 1)])
+              (let ([in-circle (unsafe-fl+ (unsafe-fl* x x) (unsafe-fl* y y))])
+                (if (unsafe-fl< in-circle 1.0)
+                    (helper (unsafe-fx+ accumulator 1) next-iter)
                     (helper accumulator next-iter))))]))
   (channel-put return-chan (helper 0 iterations)))
 
@@ -19,10 +20,10 @@
   (define (collect-from-chan count sum)
     (if (< count 1)
         sum
-        (collect-from-chan (- count 1) (+ sum (channel-get ch)))))
+        (collect-from-chan (unsafe-fx- count 1) (unsafe-fx+ sum (channel-get ch)))))
   (for ([i (in-range num-threads)])
     (thread (λ () (montecarlopi (make-pseudo-random-generator) (/ iterations num-threads) ch))))
-  (display (fl* 4.0 (fl/ (->fl (collect-from-chan num-threads 0)) (->fl iterations))))
+  (display (unsafe-fl* 4.0 (unsafe-fl/ (->fl (collect-from-chan num-threads 0)) (->fl iterations))))
   (newline))
 
 (define (main iterations num-threads)
