4ce08943a7d2913f1fb31ad862b110fc5fa01a34
James Cooper <30380166+jcoo092@users.noreply.github.com>
2020-03-31 21:55:33 +1300

    Tweaked SelectTime
    I modified SelectTime a little bit, to see if I could get it working.
    
    I have solved the problems with void parameters, or whatever the complaint was, but am still having trouble with the sync over the channels.

--- Racket/selecttime.rkt
+++ Racket/selecttime.rkt
@@ -4,32 +4,32 @@
 (require racket/random)
 
 (define (sender iterations channels)
- (match iterations
-  [0 (displayln "sender completed")]
-  [iter (
-      (let ([choice-chan (random-ref channels)])
-       (channel-put choice-chan 'message)
-  (sender (- iter 1) channels)))]
- ))
+  (match iterations
+    [0 (displayln "sender completed")]
+    [iter 
+           (let ([choice-chan (random-ref channels)])
+             (channel-put choice-chan iter)
+             (sender (- iter 1) channels))]))
 
 (define (receiver iterations channels notification-semaphore)
- (match iterations
- [0 ((displayln "receiver completed")
-     (semaphore-post notification-semaphore))]
- [iter
-  ((let ([ignored-choice (sync (choice-evt (vector->values channels)))])
-      (displayln ignored-choice)
-      (receiver (- iter 1) channels notification-semaphore)))]))
+  (match iterations
+    [0 (begin
+         (displayln "receiver completed")
+         (semaphore-post notification-semaphore))]
+    [iter
+     (let ([ignored-choice (sync (choice-evt (vector->values channels)))])
+        (begin (displayln ignored-choice)
+        (receiver (- iter 1) channels notification-semaphore)))]))
 
 (define (experiment iterations num-channels)
- (let ([channels
-        (vector->immutable-vector
-         (make-vector num-channels (make-channel)))]
-       [notification-semaphore (make-semaphore)])
-  (thread (λ () (receiver iterations channels notification-semaphore)))
-  (thread (λ () (sender iterations channels)))
-  (semaphore-wait notification-semaphore)
- ))
+  (let ([channels
+         (vector->immutable-vector
+          ;(make-vector num-channels (make-channel))
+          (build-vector num-channels (λ (i) (make-channel))))]
+        [notification-semaphore (make-semaphore)])
+    (thread (λ () (receiver iterations channels notification-semaphore)))
+    (thread (λ () (sender iterations channels)))
+    (semaphore-wait notification-semaphore)))
 
 (define (main iterations num-channels)
   (experiment (string->number iterations) (string->number num-channels))
