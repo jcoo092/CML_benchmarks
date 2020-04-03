520b3f8c630eef1126b9d654956a531df34a109d
James Cooper <30380166+jcoo092@users.noreply.github.com>
2020-04-03 19:59:52 +1300

    Started adding Kn & Grid to whispers
    Got a bit stuck though, since they need to do things to ensure they only send out one message, but receive precisely the correct number of messages...

--- Racket/whispers.rkt
+++ Racket/whispers.rkt
@@ -1,24 +1,54 @@
 #lang racket
 
 (provide main)
+(require racket/vector)
+
+(define (chans size)
+    (build-vector size (λ (i) (make-channel))))
 
 (define (ring size notification-semaphore)
+  (define channels (chans size))
+  (define (vrc idx) (vector-ref channels idx))
+  (define vrc0 (vrc 0))
   (define (receive-and-fwd in out)
-    (channel-put out (begin (displayln "hi!") (channel-get in))))
-  (define channels
-    (build-vector size (λ (i) (make-channel))))
+    (channel-put out (channel-get in)))
   ; Start off the final thread manually, since it needs to wrap around to the end of
   ; the vector for its out channel
-  (thread (λ () (receive-and-fwd (vector-ref channels (sub1 size)) (vector-ref channels 0))))
+  (thread (λ () (receive-and-fwd (vrc (sub1 size)) vrc0)))
   (for ([i (in-range (sub1 size))])
-    (thread (λ () (receive-and-fwd (vector-ref channels i) (vector-ref channels (add1 i))))))
-  (channel-put (vector-ref channels 0) 'go)
-  (channel-get (vector-ref channels 0))
+    (thread (λ () (receive-and-fwd (vrc i) (vrc (add1 i))))))
+  (channel-put vrc0 'go)
+  (channel-get vrc0)
   (semaphore-post notification-semaphore))
+
+(define (kn size notification-semaphore)
+ (define channels (chans size))
+ (define (receive-and-fwd in out)
+    (channel-put out (begin (displayln "hi!") (channel-get in))))
+ (for ([i (in-range size)])
+  (let ([events (vector-map (λ (c) (channel-put-evt c i)) channels)])
+   (vector-set! events i (channel-get (vector-ref channels i)))
+
+   ))
+ (displayln "unimplemented")
+ )
+
+(define (grid size notification-semaphore)
+ (define channels (chans size))
+ (define (receive-and-fwd in out)
+    (channel-put out (begin (displayln "hi!") (channel-get in))))
+ (displayln "unimplemented")
+ )
 
 (define (main experiment-selection size)
   ;(experiment (string->number size))
-  (define notification-semaphore (make-semaphore))
-  (ring (string->number size) notification-semaphore)
-  (semaphore-wait notification-semaphore)
+  ;(define notification-semaphore (make-semaphore))
+  ;(define size-num (string->number size))
+  (let ([notification-semaphore (make-semaphore)]
+        [size-num (string->number size)])
+  (match (string-downcase (string-trim experiment-selection))
+   ["ring" (ring size-num notification-semaphore)]
+   ["kn" (kn size-num notification-semaphore)]
+   ["grid" (grid size-num notification-semaphore)])
+  (semaphore-wait notification-semaphore))
   (displayln (string-append experiment-selection " of Whispers completed successfully")))
