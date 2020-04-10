c8f984837c11c0f7a47f13a1114f456e0f0b0099
James Cooper <30380166+jcoo092@users.noreply.github.com>
2020-04-10 20:44:04 +1200

    Make Kn of Whispers work
    Got a version going that basically works correctly.  Mostly.  I think.
    
    The big remaining problem seems to be that it looks like the communicate calls aren't necessarily tail-calls for some reason...
    
    Plus, I still have to fix up grid and ring, and there's a load of cruft throughout it now.  E.g. the use of the semaphore.

--- Racket/whispers.rkt
+++ Racket/whispers.rkt
@@ -25,60 +25,61 @@
 
 (define (communicate notification-semaphore ch count comms-events)
   ;(printf "count: ~v, length: ~a, events: ~v\n" count (length comms-events) comms-events)
-  (if (and (zero? count) (null? comms-events))
-      (begin (displayln "Finished communicate") (semaphore-post notification-semaphore))
-      ((let* ([evs (flatten (list* ch comms-events))]
+  (let ([zc (zero? count)] [nc (null? comms-events)])
+    (cond
+      [(and zc nc) (semaphore-post notification-semaphore)]
+      [zc (let ([result (apply sync comms-events)])
+            (communicate notification-semaphore ch count (remq result comms-events)))]
+      [nc (begin (sync ch)
+                 (communicate notification-semaphore ch (sub1 count) comms-events))]
+      [else (let* ([evs (list* ch comms-events)]
+                   [result
+                    ;(wrap-evt ch (λ (e) (begin (displayln (number->string e)) (e))))
+                    (apply sync evs)])
+              ;(displayln (~v result))
+              (if (channel-put-evt? result)
+                  (communicate notification-semaphore ch count (remq result comms-events))
+                  (communicate notification-semaphore ch (sub1 count) comms-events)))]))
+  #| (if (and (zero? count) (null? comms-events))
+     (semaphore-post notification-semaphore)
+      ((let* ([evs (list* ch comms-events)]
               [result
-;                      (wrap-evt ch (λ (e) (begin (displayln (number->string e)) (e))))
-                      (apply sync evs)])
+               ;(wrap-evt ch (λ (e) (begin (displayln (number->string e)) (e))))
+               (apply sync evs)])
          ;(displayln (~v result))
          (if (channel-put-evt? result)
-             ((printf "is result in comms-events? ~v\n" (ormap (curry eq? result) comms-events))
-              (communicate notification-semaphore ch count (remq result comms-events)))
-             (communicate notification-semaphore ch (sub1 count) comms-events))))))
+             (communicate notification-semaphore ch count (remq result comms-events))
+             (communicate notification-semaphore ch (sub1 count) comms-events))))) |#
+  (displayln "Finished communicate"))
 
 (define (kn size notification-semaphore)
-  #| (define (communicate ch count comms-events)
-   (displayln (number->string count))
-    (if (and (zero? count) (null? comms-events))
-        (begin (displayln "Finished communicate") (semaphore-post notification-semaphore))
-        ((let ([result (sync
-                        (wrap-evt ch (λ (e) (begin (displayln (number->string e)) (e))))
-                        (apply comms-events))])
-          (if (channel-put-evt? result)
-            (communicate count (remq result comms-events))
-            (communicate (sub1 count) comms-events)))))) |#
   (define channels (chans size))
-  #| (define (receive-and-fwd in out)
-    (channel-put out (channel-get in))) |#
-  (for ([i (in-range size)])
-    (let* ([events (map (λ (c) (channel-put-evt c i)) channels)]
-           [events2 (list* (take events i) (drop events (add1 i)))])
-      ;(list-set events i (channel-get (list-ref channels i))) ; Need to change this to drop the relevant event
-      (thread (λ () (communicate notification-semaphore (list-ref channels i) (sub1 size)
-                                 events2)))))
-  (displayln "started kn"))
+  (define threads
+    (for/list ([i (in-range size)])
+      (let* ([events (map (λ (c) (channel-put-evt c i)) channels)]
+             [events2 (flatten (list* (take events i) (drop events (add1 i))))])
+        (printf "~v\n" events2)
+        (thread (λ () (communicate notification-semaphore (list-ref channels i) (sub1 size)
+                                   events2))))))
+  ;(printf "~v\n" threads)
+  (displayln "started kn")
+  (for-each thread-wait threads))
 
 ; TODO:  Implement grid (which will probably largely be a copy of kn)
 (define (grid size notification-semaphore)
   (define channels (chans size))
   (define (receive-and-fwd in out)
     (channel-put out (begin (displayln "hi!") (channel-get in))))
-  (displayln "unimplemented")
-  )
+  (displayln "unimplemented"))
 
-; TODO:  Modify main (or experiment) to work with semaphores with a size > 1
 ; TODO:  Change program to work with multiple iterations of communication
 (define (main experiment-selection iterations size)
-  ;(experiment (string->number size))
   ;(define notification-semaphore (make-semaphore))
-  ;(define size-num (string->number size))
   ;(displayln experiment-selection)
   (let* ([size-num (string->number size)]
          [notification-semaphore (make-semaphore size-num)])
     (match (string-downcase (string-trim experiment-selection))
       ["ring" (ring size-num notification-semaphore)]
       ["kn" (kn size-num notification-semaphore)]
-      ["grid" (grid size-num notification-semaphore)])
-    (semaphore-wait notification-semaphore))
+      ["grid" (grid size-num notification-semaphore)]))
   (displayln (string-append experiment-selection " of Whispers completed successfully")))
