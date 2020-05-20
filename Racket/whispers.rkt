5ccd9e98a158b8fa55cd14b541998a7fb1b9aaa2
James Cooper <30380166+jcoo092@users.noreply.github.com>
2020-05-20 14:17:24 +1200

    Tidy Whispers.rkt

--- Racket/whispers.rkt
+++ Racket/whispers.rkt
@@ -1,6 +1,5 @@
 #lang racket/base
 
-#;(provide main)
 (require racket/match racket/list racket/string racket/function)
 (require racket/place racket/future)
 
@@ -18,26 +17,6 @@
       (vector-set! ret-vec i (add1 curr-val))))
   ret-vec)
 
-; My function to collect a bunch of the ends from place channel creation into two separate lists.
-#;
-(define (create-place-chans length)
-  (define (helper iteration rxes txes)
-    (match iteration
-      [0 (values rxes txes)]
-      [iter
-       (let-values ([(rx tx) (place-channel)])
-         (helper (sub1 iteration) (list* rx rxes) (list* tx txes)))]))
-  (helper length null null))
-
-; Alternatives suggested by @sorawee in the Racket slack.  Thanks!
-#;
-(define (create-place-chans length)
-  (let loop ([iteration length] [rxes null] [txes null])
-    (match iteration
-      [0 (values rxes txes)]
-      [_ (define-values (rx tx) (place-channel))
-         (loop (sub1 iteration) (cons rx rxes) (cons tx txes))])))
-
 (define (create-place-chans length)
   (for/fold ([rxes null] [txes null] #:result (values rxes txes)) 
       ([iteration (in-range length)])
@@ -50,33 +29,16 @@
   (define new-front (drop l permutation-size))
   (flatten (list new-front new-back)))
 
-;***************************
-
-#;
-(define (ring size notification-semaphore)
-  (define channels (chans-vector size))
-  #;(define (vrc idx) (vector-ref channels idx))
-  (define (vrc) (curry vector-ref channels))
-  (define vrc0 (vrc 0))
-  (define (receive-and-fwd in out)
-    (channel-put out (channel-get in)))
-  ; Start off the final thread manually, since it needs to wrap around to the end of
-  ; the vector for its out channel
-  (thread (λ () (receive-and-fwd (vrc (sub1 size)) vrc0)))
-  (for ([i (in-range (sub1 size))])
-    (thread (λ () (receive-and-fwd (vrc i) (vrc (add1 i))))))
-  (channel-put vrc0 'go)
-  (channel-get vrc0)
-(semaphore-post notification-semaphore))
+                                        ;***************************
+                                        ;********* Ring ************
 
 (define (run-thread in out)
   (channel-put out (channel-get in))
-  #;(displayln "Thread just forwarded its message!")
+
   (run-thread in out))
 
 (define (run-place rx tx start-chan end-chan)    
   (let ([message (place-channel-get rx)])
-    #;(printf "This place just got a message! Message was ~a\n" message)
     (match message
       [0 (begin
            (place-channel-put tx message)
@@ -94,22 +56,15 @@
    (let ([ch-vec (chans-vector num-threads)]
          [end-chan (make-channel)])
      (for ([i (in-range (sub1 num-threads))])
-       (thread (λ () (run-thread (vector-ref ch-vec i) (vector-ref ch-vec (add1 i)))))
-       ;(displayln "Started a thread!")
-       )
+       (thread (λ () (run-thread (vector-ref ch-vec i) (vector-ref ch-vec (add1 i))))))
      (thread (λ () (run-thread (vector-ref ch-vec (sub1 num-threads)) end-chan)))
-     ;(displayln "Started the last thread!")
-     ;(displayln "Started place!")
      (run-place rx tx (vector-ref ch-vec 0) end-chan))))
 
 (define (interpose rx tx)
-  #;(displayln "Started interpose!")
   (match (place-channel-get rx)
     [0 (displayln "Job's done!")]
     [i (begin
-         #;(displayln "Matched!")
          (place-channel-put tx (sub1 i))
-         #;(displayln "Interposed!")
          (interpose rx tx))]))
 
 (define (ring/place iterations size num-places)
@@ -121,7 +76,8 @@
     (interpose (first rxes) (first perm-txes))
     (for-each place-wait places)))
 
-;***************************
+                                        ;***************************
+                                        ;************ Kn ***********
 
 (define (communicate ch count comms-events)
   (let ([zc (zero? count)] [nc (null? comms-events)])
@@ -148,7 +104,9 @@
   (displayln "started kn")
   (for-each thread-wait threads))
 
-;***************************
+                                        ;***************************
+                                        ;*********** Grid **********
+
 
 (define (compute-neighbour-indices width idx)
   (let-values ([(y x) (quotient/remainder idx width)])
@@ -175,22 +133,13 @@
 
 ;***************************
 
-; TODO:  Change program to work with multiple iterations of communication
-#;(define (main experiment-selection iterations size [width "50"] [height "50"])
-  (let* ([size-num (string->number size)]
-         [notification-semaphore (make-semaphore size-num)])
-    (match (string-downcase (string-trim experiment-selection))
-      ["ring" (ring size-num notification-semaphore)]
-      ["kn" (kn size-num)]
-      ["grid" (grid (string->number width) (string->number height))]))
-(displayln (string-append experiment-selection " of Whispers completed successfully")))
-
 (define (get-numerical-param-from-vec-or-default default vec param-pos)
   (if (< param-pos (vector-length vec))
       (string->number (vector-ref vec param-pos))
       default))
 
 (module+ main
+  (define default-width 50)
   (define cmd-params (current-command-line-arguments))
   (let* ([experiment-selection (string-trim (vector-ref cmd-params 0))]
          [iterations (string->number (vector-ref cmd-params 1))]
@@ -201,6 +150,6 @@
       ["ring" (ring/place iterations size-num num-places)]
       ["kn" (kn iterations size-num num-places)]
       ["grid" (begin
-                (define (gnp) (curry get-numerical-param-from-vec-or-default 50 cmd-params))
+                (define (gnp) (curry get-numerical-param-from-vec-or-default default-width cmd-params))
                 (grid iterations (gnp 3) (gnp 4) num-places))])
     (displayln (string-append experiment-selection " of Whispers completed successfully"))))
