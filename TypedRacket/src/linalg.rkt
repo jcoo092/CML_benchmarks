e56864df0f088bf421881925a97bf0645602d167
James Cooper <30380166+jcoo092@users.noreply.github.com>
2020-06-12 21:10:29 +1200

    Finish linalg.rkt (best as I can tell).  Importantly, this includes a fix to ensure that the program actually runs, instead of before where it was starting up, but not actually processing anything...

--- TypedRacket/src/linalg.rkt
+++ TypedRacket/src/linalg.rkt
@@ -1,7 +1,8 @@
 #lang typed/racket
 
-(provide main)
 (require math/matrix math/array)
+
+                                        ;*************************** Common ***************************
 
 (define max-val 256)
 
@@ -19,46 +20,46 @@
 (define (rem-all-by-256 vec)
   (array-map (λ (m) (remainder-or-random m)) vec))
 
-;***************************
+                                        ;*************************** Vector ***************************
 
-; Racket's Math library doesn't seem to make any distinction between vectors and matrices
+                                        ; Racket's Math library doesn't seem to make any distinction between vectors and matrices
                                         ; Vectors are just matrices where one of the dimensions has size 1
 (: vector (-> Nonnegative-Fixnum Nonnegative-Fixnum Void))
 (define (vector iterations size)
 
-  (: initvector1 (Array Nonnegative-Fixnum))
+  (: initvector1 (Matrix Nonnegative-Fixnum))
   (define initvector1 (build-matrix 1 size (λ (a b) (rando))))
-  (: initvector2 (Array Nonnegative-Fixnum))
+  (: initvector2 (Matrix Nonnegative-Fixnum))
   (define initvector2 (build-matrix 1 size (λ (a b) (rando))))
 
-  (: process-vectors (-> Nonnegative-Fixnum (Array Nonnegative-Fixnum) (Array Nonnegative-Fixnum) Void))
+  (: process-vectors (-> Nonnegative-Fixnum (Matrix Nonnegative-Fixnum) (Matrix Nonnegative-Fixnum) Void))
   (define (process-vectors iteration v1 v2)
     (if (zero? iteration)
         (void)
-        (let ([pluses (cast (matrix+ v1 v2) (Array Nonnegative-Fixnum))]
-              [times (cast (matrix-row (matrix* (matrix-transpose v1) v2) 0) (Array Nonnegative-Fixnum))])
+        (let ([pluses (cast (matrix+ v1 v2) (Matrix Nonnegative-Fixnum))]
+              [times (cast (matrix-row (matrix* (matrix-transpose v1) v2) 0) (Matrix Nonnegative-Fixnum))])
           (process-vectors (sub1 iteration) (rem-all-by-256 pluses) (rem-all-by-256 times)))))
   (process-vectors iterations initvector1 initvector2))
 
-;***************************
+                                        ;*************************** Matrix ***************************
 
 (: matrixops (-> Nonnegative-Fixnum Nonnegative-Fixnum Void))
 (define (matrixops iterations size)
   (define initmatrix1 (build-matrix size size (λ (a b) (rando))))
   (define initmatrix2 (build-matrix size size (λ (a b) (rando))))
 
-  (: process-matrices (-> Nonnegative-Fixnum (Array Nonnegative-Fixnum) (Array Nonnegative-Fixnum) Void))
+  (: process-matrices (-> Nonnegative-Fixnum (Matrix Nonnegative-Fixnum) (Matrix Nonnegative-Fixnum) Void))
   (define (process-matrices iteration m1 m2)
     (if (zero? iteration)
         (void)
-        (let ([pluses (cast (matrix+ m1 m2) (Array Nonnegative-Fixnum))]
-              [times (cast (matrix* m1 m2) (Array Nonnegative-Fixnum))])
+        (let ([pluses (cast (matrix+ m1 m2) (Matrix Nonnegative-Fixnum))]
+              [times (cast (matrix* m1 m2) (Matrix Nonnegative-Fixnum))])
           (process-matrices (sub1 iteration) (rem-all-by-256 times) (rem-all-by-256 pluses)))))
   (process-matrices iterations initmatrix1 initmatrix2))
 
-;***************************
+                                        ;*************************** Mixed ***************************
 
-; This test should check both row and column vector multiplication against square matrices
+                                        ; This test should check both row and column vector multiplication against square matrices
 
 (: mixed (-> Nonnegative-Fixnum Nonnegative-Fixnum Void))
 (define (mixed iterations size)
@@ -66,26 +67,26 @@
   (define initrowvec (build-matrix 1 size (λ (a b) (rando))))
   (define initmatrix1 (build-matrix size size (λ (a b) (rando))))
 
-  (: process-mixed (-> Nonnegative-Fixnum (Array Nonnegative-Fixnum) (Array Nonnegative-Fixnum) (Array Nonnegative-Fixnum) Void))
+  (: process-mixed (-> Nonnegative-Fixnum (Matrix Nonnegative-Fixnum) (Matrix Nonnegative-Fixnum) (Matrix Nonnegative-Fixnum) Void))
   (define (process-mixed iteration colvec rowvec m)
     (if (zero? iteration)
         (void)
         (let* ([next-iter (sub1 iteration)]
-               [next-colvec (rem-all-by-256 (cast (matrix* m colvec) (Array Nonnegative-Fixnum)))]
-               [next-rowvec (rem-all-by-256 (cast (matrix* rowvec m) (Array Nonnegative-Fixnum)))]
-               [next-matrix (rem-all-by-256 (cast (matrix* next-colvec next-rowvec) (Array Nonnegative-Fixnum)))])
+               [next-colvec (rem-all-by-256 (cast (matrix* m colvec) (Matrix Nonnegative-Fixnum)))]
+               [next-rowvec (rem-all-by-256 (cast (matrix* rowvec m) (Matrix Nonnegative-Fixnum)))]
+               [next-matrix (rem-all-by-256 (cast (matrix* next-colvec next-rowvec) (Matrix Nonnegative-Fixnum)))])
           (process-mixed next-iter next-colvec next-rowvec next-matrix))))
   (process-mixed iterations initcolvec initrowvec initmatrix1))
 
-;***************************
+                                        ;***************************
 
-(: main (-> String String String Void))
-(define (main experiment-selection iterations size)
-  (let ([size-num (cast (string->number size) Nonnegative-Fixnum)]
-        [iter-num (cast (string->number iterations) Nonnegative-Fixnum)])
-    (cast iter-num Nonnegative-Fixnum)
-    (case (string-downcase (string-trim experiment-selection))
-      [("vector") (vector iter-num size-num)]
-      [("matrix") (matrixops iter-num size-num)]
-      [("mixed") (mixed iter-num size-num)]))
-  (displayln (string-append experiment-selection " completed successfully")))
+(module+ main
+  (define cmd-params (current-command-line-arguments))
+  (define experiment-selection (vector-ref cmd-params 0))
+  (define iter-num (cast (string->number (vector-ref cmd-params 1)) Nonnegative-Fixnum))
+  (define size-num (cast (string->number (vector-ref cmd-params 2)) Nonnegative-Fixnum))
+  (case (string-downcase (string-trim experiment-selection))
+    [("vector") (vector iter-num size-num)]
+    [("matrix") (matrixops iter-num size-num)]
+    [("mixed") (mixed iter-num size-num)])
+  (displayln (string-append experiment-selection " completed successfully!")))
