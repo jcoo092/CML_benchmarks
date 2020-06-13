84370310db30b3b3edc9b29e8722e860eb2bfbac
James Cooper <30380166+jcoo092@users.noreply.github.com>
2020-06-13 20:17:43 +1200

    MonteCarloPi: Start on Typed conversion.

--- TypedRacket/src/montecarlopi.rkt
+++ TypedRacket/src/montecarlopi.rkt
@@ -1,17 +1,19 @@
-#lang racket/base
+#lang typed/racket/base
 
-(require racket/match)
-(require racket/flonum racket/unsafe/ops)
+(require racket/match racket/fixnum racket/flonum)
 (require racket/place racket/future)
+(require/typed racket/unsafe/ops [unsafe-fxvector-length (FxVector -> Fixnum)])
 
+(: distribute-extras (-> Nonnegative-Fixnum Nonnegative-Fixnum FxVector))
 (define (distribute-extras total base)
-  (define ret-vec (make-vector base (quotient total base)))
-  (define leftovers (remainder total base))
+  (define ret-vec (make-fxvector base (fxquotient total base)))
+  (define leftovers (fxremainder total base))
   (for ([i (in-range leftovers)])
-    (let ([curr-val (vector-ref ret-vec i)])
-      (vector-set! ret-vec i (add1 curr-val))))
+    (let ([curr-val (fxvector-ref ret-vec i)])
+      (fxvector-set! ret-vec i (fx+ 1 curr-val))))
   ret-vec)
 
+(: montecarlopi/place (-> Nonnegative-Fixnum Nonnegative-Fixnum Place-Channel Place))
 (define (montecarlopi/place iterations num-threads return-chan)
   (place/context
    c
@@ -21,30 +23,33 @@
          (match iteration
            [0 accumulator]
            [iter (let ([x (random randomiser)] [y (random randomiser)]
-                       [next-iter (unsafe-fx- iter 1)])
-                   (let ([in-circle (unsafe-fl+ (unsafe-fl* x x) (unsafe-fl* y y))])
-                     (if (unsafe-fl< in-circle 1.0)
-                         (helper (unsafe-fx+ accumulator 1) next-iter)
+                       [next-iter (fx- iter 1)])
+                   (let ([in-circle (fl+ (fl* x x) (fl* y y))])
+                     (if (fl< in-circle 1.0)
+                         (helper (fx+ accumulator 1) next-iter)
                          (helper accumulator next-iter))))]))
        (place-channel-put return-chan (helper 0 thread-iterations)))
      (map sync (for/list ([i (distribute-extras iterations num-threads)])
                  (thread (λ () (run-thread-in-place (make-pseudo-random-generator) i))))))))
 
+(: experiment (-> Nonnegative-Fixnum Nonnegative-Fixnum Void))
 (define (experiment iterations num-threads)
-  (define num-cores (processor-count))
-  (define threads-per-place-vec (distribute-extras num-threads num-cores))
-  (define iters-per-place-vec (distribute-extras iterations num-cores))
+  (define num-cores (cast (processor-count) Nonnegative-Fixnum))
+  (define threads-per-place-vec (assert (distribute-extras num-threads num-cores) fxvector?))
+  (define iters-per-place-vec (assert (distribute-extras iterations num-cores) fxvector?))
   (define-values (rx-ch tx-ch) (place-channel))
+
+  (: collect-from-chan (-> Nonnegative-Fixnum Nonnegative-Fixnum Nonnegative-Fixnum))
   (define (collect-from-chan count sum)
     (if (< count 1)
         sum
         (collect-from-chan (sub1 count)
-                           (unsafe-fx+ sum (place-channel-get rx-ch)))))
-  (for ([ts (in-vector threads-per-place-vec)]
-        [is (in-vector iters-per-place-vec)])
+                           (fx+ sum (cast (place-channel-get rx-ch) Nonnegative-Fixnum)))))
+  (for ([ts (in-fxvector threads-per-place-vec)]
+        [is (in-fxvector iters-per-place-vec)])
     (montecarlopi/place is ts tx-ch))
 
-  (displayln (unsafe-fl* 4.0 (unsafe-fl/ ;
+  (displayln (fl* 4.0 (fl/ ;
                               (->fl (collect-from-chan num-threads 0)) ;
                               (->fl iterations))))
   (displayln "Monte Carlo Pi completed successfully"))
