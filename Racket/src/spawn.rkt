0eb9473e34b3dcac2a77a9d6bcd65eaea70458aa
James Cooper <30380166+jcoo092@users.noreply.github.com>
2020-06-14 16:20:33 +1200

    Change spawn.rkt to spawn places only once

--- Racket/src/spawn.rkt
+++ Racket/src/spawn.rkt
@@ -6,20 +6,35 @@
   #;(printf "I am child thread ~a of place ~a, during iteration ~a\n" thread-id place-id iteration)
   (void))
 
-(define (child/place iteration id num-threads)
+#;(define (child/place iteration id num-threads)
   (place/context
    c
    (let ([threads-list (for/list ([i (in-range num-threads)])
                          (thread (λ () (child iteration id i))))])
      (map thread-wait threads-list))))
 
-(define (experiment iterations num-threads)
+#;(define (experiment iterations num-threads)
   (define num-cores (processor-count))
   (define threads-per-place (max 1 (quotient num-threads num-cores)))
   (for ([i (in-range iterations)])
     (let ([places (for/list ([j (in-range num-cores)])
                     (child/place i j threads-per-place))])
       (map place-wait places))))
+
+(define (child/place iterations id num-threads)
+  (place/context
+   c
+   (for ([i (in-range iterations)])
+       (let ([threads-list (for/list ([t (in-range num-threads)])
+                             (thread (λ () (child i id t))))])
+         (map thread-wait threads-list)))))
+
+(define (experiment iterations num-threads)
+  (define num-cores (processor-count))
+  (define threads-per-place (max 1 (quotient num-threads num-cores)))
+  (let ([places (for/list ([j (in-range num-cores)])
+                  (child/place iterations j threads-per-place))])
+    (map place-wait places)))
 
 (module+ main
   (define cmd-params (current-command-line-arguments))
